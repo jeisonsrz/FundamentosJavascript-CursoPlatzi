7f7502db9bf7e48f7c76d9f582a3d1bfe4a02703
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2018-09-18 18:18:57 -0500

    Clase 4 - Funciones ⏳

---
+++ clase4/clase4.js
@@ -0,0 +1,13 @@
+var nombre = 'Jeison', edad = 22 ;
+
+function imprimirEdad(n,e) {
+
+    console.log(`${n} tiene ${e} años`);
+
+}
+
+imprimirEdad(nombre,edad);
+
+imprimirEdad('poncho',4);
+imprimirEdad('viky',34);
+imprimirEdad('camilo',5);
