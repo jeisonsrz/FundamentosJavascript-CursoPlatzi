b9f590ca9561fecdc292855ae4733a1d78fe9089
jeisonsrz <jeison.ruiz.z@uniautonoma.edu.co>
2018-09-18 20:42:36 -0500

    Clase 8 y clase 9 ⚡

---
+++ clase8/clase8.js
@@ -0,0 +1,45 @@
+var jeison = {
+
+    nombre : 'jeison',
+    apellido : 'Ruiz',
+    edad : 28
+}
+
+var dario = {
+
+    nombre : 'Dario',
+    apellido : 'susnisky',
+    edad : 32
+
+}
+function imprimirNombreenMayusculas(persona) {
+    // var nombre = persona.nombre;
+    var {nombre}= persona;
+   console.log(nombre.toUpperCase());
+
+}
+
+imprimirNombreenMayusculas(jeison);
+imprimirNombreenMayusculas(dario);
+imprimirNombreenMayusculas({nombre : 'pepito'});
+
+
+//Solucion RETO nombre y edad
+
+function imprimirNombreyEdad(persona) {
+     var {nombre,edad} = persona;
+   console.log(`Hola mi nombre es ${nombre}  y tengo ${edad} años.`);
+  
+
+}
+
+imprimirNombreyEdad(jeison);
+imprimirNombreyEdad(dario);
+
+function cumpleaños(persona) {
+    return{
+        ...persona,
+       edad : persona.edad +2
+    }
+   
+}
---
+++ clase9/clase9.js
@@ -0,0 +1,9 @@
+var x = 4 , y = '4' ;
+
+var jeison = {
+    nombre : 'jeison'
+}
+
+var otraPersona = {
+   ...jeison
+}
